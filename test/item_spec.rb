16a1352e7db9ee9fb963a57b25ec8c24b693c78a
arifk-repo <123160068@student.upnyk.ac.id>
2021-07-31 21:57:13 +0700

    show form to add category off item

--- test/item_spec.rb
+++ test/item_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require '../models/item'
+require '../models/category'
 require '../controllers/item_controller'
 require 'erb'
 
@@ -21,6 +22,14 @@ describe ItemController do
       actual_view = @item_controller.list_category(id)
       category = Item.find_category(id)
       expected_output = ERB.new(File.read("../views/list_category.erb")).result(binding)
+      expect(expected_output).to eq(actual_view)
+    end
+
+    it "Show form to add categories item" do
+      id=1
+      actual_view = @item_controller.form_add_category(id)
+      categories = Category.get_all_categories
+      expected_output = ERB.new(File.read("../views/add_category.erb")).result(binding)
       expect(expected_output).to eq(actual_view)
     end
   end
