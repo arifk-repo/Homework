0e0484e07944e1201b7d90acba4d834146a9d86a
arifk-repo <123160068@student.upnyk.ac.id>
2021-07-31 21:21:44 +0700

    Edit Category

--- test/category_spec.rb
+++ test/category_spec.rb
@@ -21,5 +21,20 @@ describe CategoryController do
       expected_output = ERB.new(File.read("../views/create.erb")).result(binding)
       expect(expected_output).to eq(actual_view)
     end
+
+    it "Show form to add" do
+      actual_view = @category_controller.form_add
+      expected_output = ERB.new(File.read("../views/new_category.erb")).result(binding)
+      expect(expected_output).to eq(actual_view)
+    end
+
+    it "Edit Category" do
+      id=5
+      actual_view = @category_controller.edit_category(id)
+      category = Category.find_by_id(id)
+      expected_output = ERB.new(File.read("../views/edit_category.erb")).result(binding)
+      expect(expected_output).to eq(actual_view)
+    end
+
   end
 end
