59c0ecfe02abf592c6721a7c2d8ab6bc66161b80
arifk-repo <123160068@student.upnyk.ac.id>
2021-07-31 21:54:38 +0700

    get all category of  item

--- test/item_spec.rb
+++ test/item_spec.rb
@@ -9,12 +9,19 @@ describe ItemController do
       @item_controller = ItemController.new
     end
 
-    it "get all category from database" do
+    it "get all item" do
       actual_view = @item_controller.index
       item = Item.find_all
       expected_output = ERB.new(File.read("../views/index.erb")).result(binding)
       expect(expected_output).to eq(actual_view)
     end
 
+    it "get all list item" do
+      id =1
+      actual_view = @item_controller.list_category(id)
+      category = Item.find_category(id)
+      expected_output = ERB.new(File.read("../views/list_category.erb")).result(binding)
+      expect(expected_output).to eq(actual_view)
+    end
   end
 end
